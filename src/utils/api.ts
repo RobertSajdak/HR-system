9c062c0893bea0438ca56b255770b8f6fb7c4f5a
RobertSajdak <robert.sajdak@onet.eu>
2025-01-14 16:31:21 +0100

    API communication logic added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,13 @@
-export const saveAttendance = async (data: 'any') => {
-    console.log("Saving data...", data);
+import { AttendanceData } from "../types/attendance";
+
+export const saveAttendance = async (data: AttendanceData) => {
+    const response = await fetch("/api/attendance", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to save attendance data");
+    }
 };
