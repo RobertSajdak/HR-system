5d3a33c6202fe036f6616f5bdb4dc50c408f5b75
RobertSajdak <robert.sajdak@onet.eu>
2025-01-14 16:36:01 +0100

    local API changed: /pages/api/attendance.ts

--- src/pages/api/attendance.ts
+++ src/pages/api/attendance.ts
@@ -2,7 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
-        res.status(200).json({ message: "Data received" });
+        console.log(req.body); // Symulacja zapisu danych
+        res.status(200).json({ message: "Data saved successfully" });
     } else {
         res.setHeader("Allow", ["POST"]);
         res.status(405).end(`Method ${req.method} Not Allowed`);
